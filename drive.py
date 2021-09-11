738d29041e688dace6147198643127846ff8f399
Raphael <raphael.ugs@gmail.com>
2021-09-11 10:04:37 -0300

    Fix expired token auto refresh

--- drive.py
+++ drive.py
@@ -9,6 +9,7 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google.auth.exceptions import RefreshError
 from google.oauth2.credentials import Credentials
 
 from auxiliar import *
@@ -38,9 +39,14 @@ class Drive:
             creds = Credentials.from_authorized_user_file(AUTH_TOKEN_FILE,
                 AUTH_SCOPES)
         if not creds or not creds.valid:
+            refreshed = False
             if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
+                try:
+                    creds.refresh(Request())
+                    refreshed = True
+                except RefreshError:
+                    os.path.remove(AUTH_TOKEN_FILE)
+            if not refreshed:
                 flow = InstalledAppFlow.from_client_secrets_file(
                     secret_file, AUTH_SCOPES)
                 creds = flow.run_local_server(port=0)
