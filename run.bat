228baeb070bc31c3a1fb133deb63f94201cacfc1
Raphael <raphael.ugs@gmail.com>
2022-04-05 19:16:52 -0300

    No max size option in example script

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 
-venv\Scripts\python.exe upload.py --ask-source --ask-dest --dest "fotos-videos"
+venv\Scripts\python.exe upload.py --ask-source --ask-dest --dest "fotos-videos" --no-max-size
 rem venv\Scripts\python.exe checkdups.py --ask-dest --dest "fotos-videos"
 pause
